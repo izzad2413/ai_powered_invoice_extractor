ace407806412b2f33a854979cbcee921695a00a3
izzad2413 <nazmirulizzadnassir@gmail.com>
2025-04-22 12:49:13 +0800

    debug installation

---
+++ install.bat
@@ -0,0 +1,3 @@
+winget install Microsoft.VisualStudio.2022.BuildTools --force --override "--passive --wait --add Microsoft.VisualStudio.Workload.VCTools;includeRecommended"
+
+rem https://learn.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-build-tools?view=vs-2022
